13a1a3cff278dec898424e62b1d4774526828d82
Anand Kathiresan <aillanan@yahoo.com>
2018-03-26 19:23:33 +0530

    initial development

---
+++ src/VisualOdometry/VisualOdometry/VisualOdometer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualOdometry
+{
+    public class VisualOdometer : IDisposable
+    {
+
+
+        public void Dispose()
+        {
+            OdometerSettings.Default.GroundRegionTop = m_GroundRegionTop;
+            OdometerSettings.Default.SkyRegionBottom = m_SkyRegionBottom;
+            OdometerSettings.Default.Save();
+
+            if (this.OpticalFlow != null)
+            {
+                this.OpticalFlow.Dispose();
+            }
+        }
+    }
+}
